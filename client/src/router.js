1c5d813569a723dfcad885559c0153c064574f40
Redouane Achouri <achouri.a.r@gmail.com>
2018-12-31 20:48:33 +0100

    Added Signup functionality

--- client/src/router.js
+++ client/src/router.js
@@ -1,17 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Signup from './views/Signup.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   mode: 'history',
   // base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: { title: 'Project Manager' }
+    },
+    {
+      path: '/signup/:id',
+      name: 'signup',
+      component: Signup,
+      meta: { title: 'Signup - Project Manager' }
     }
   ]
 })
+
+router.afterEach((to, from) => {
+  document.title = to.meta.title
+})
+
+export default router
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,40 +1,40 @@
 <template>
-  <el-container>
-    <el-header>
-    </el-header>
+    <el-container>
+        <el-header>
+        </el-header>
 
-    <el-main>
-      <div class="container-center">
-        <h2>Welcome!</h2>
-        <div>Enter your email address to start free trial</div>
+        <el-main>
+            <div class="container-center">
+                <h2>Welcome!</h2>
+                <div>Enter your email address to start a free trial</div>
 
-        <div v-if="error" class="error">
-          {{ error }}
-        </div>
+                <div v-if="error" class="error">
+                    {{ error }}
+                </div>
 
-        <el-form ref="form" :model="form">
-          <el-form-item>
-            <label>Email</label>
-            <el-input v-model="form.email" placeholder="Email"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="capture">Create my account</el-button>
-          </el-form-item>
-        </el-form>
+                <el-form ref="form" :model="form">
+                    <el-form-item>
+                        <label>Email</label>
+                        <el-input v-model="form.email" placeholder="Email"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="capture">Create my account</el-button>
+                    </el-form-item>
+                </el-form>
 
-        <div>
-          <span>Already have an account?</span>
-          <router-link :to="{name: 'login'}" class="link">Log in</router-link>
-        </div>
+                <div>
+                    <span>Already have an account?</span>
+                    <router-link :to="{name: 'login'}" class="link">Log in</router-link>
+                </div>
 
-        <div v-if="submitted">
-          <div>Thank you!</div>
-          <div>Please check your email.</div>
-        </div>
-      </div>
-    </el-main>
+                <div v-if="submitted">
+                    <div>Thank you!</div>
+                    <div>Please check your email.</div>
+                </div>
+            </div>
+        </el-main>
 
-  </el-container>
+    </el-container>
 </template>
 
 
@@ -43,53 +43,53 @@ import { CaptureEmail } from '../constants/query.gql'
 import { validateEmail } from '@/helpers/helpers'
 
 export default {
-  data() {
-    return {
-      submitted: false,
-      error: false,
-      form: {
-        email: '',
-      }
-    }
-  },
-
-  methods: {
-    capture() {
-      const {email} = this.form
-      if (!email || !validateEmail(email)) {
-        this.error = "Please enter a valide email"
-        return
-      }
-
-      this.$apollo.mutate({
-        mutation: CaptureEmail,
-        variables: {email}
-      }).then(({data}) => {
-        this.submitted = true
-        this.error = false
-
-        // For development only
-        console.log(data.captureEmail.id)
-      }).catch((error) => {
-        if (error.graphQLErrors.length > 0) {
-          this.error = error.graphQLErrors[0].message
-        } else {
-          this.error = 'Something went wrong'
+    data() {
+        return {
+            submitted: false,
+            error: false,
+            form: {
+                email: '',
+            }
         }
-        console.log(this.error)
-      })
+    },
+
+    methods: {
+        capture() {
+            const { email } = this.form
+            if (!email || !validateEmail(email)) {
+                this.error = "Please enter a valide email"
+                return
+            }
+
+            this.$apollo.mutate({
+                mutation: CaptureEmail,
+                variables: { email }
+            }).then(({ data }) => {
+                this.submitted = true
+                this.error = false
+
+                // For development only
+                console.log(data.captureEmail.id)
+            }).catch((error) => {
+                if (error.graphQLErrors.length > 0) {
+                    this.error = error.graphQLErrors[0].message
+                } else {
+                    this.error = 'Something went wrong'
+                }
+                console.log(this.error)
+            })
+        }
     }
-  }
 }
 </script>
 
 
 <style lang="scss" scoped>
 .el-button {
-  width: 100%
+    width: 100%
 }
 
 .error {
-  padding-top: 10px
+    padding-top: 10px
 }
 </style>
